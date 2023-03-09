3763ce00cb135064f8802647418b1112ea714b24
Denis <yadeniskotcvet@gmail.com>
2023-03-09 21:27:07 +0700

    add tests

--- tests/Zomp.EFCore.WindowFunctions.Npgsql.Tests/RankTests.cs
+++ tests/Zomp.EFCore.WindowFunctions.Npgsql.Tests/RankTests.cs
@@ -21,8 +21,17 @@ public class RankTests : TestBase
     public void RankBasic() => rankTests.RankBasic();
 
     [Fact]
+    public void RankEmptyOver() => rankTests.RankEmptyOver();
+
+    [Fact]
     public void DenseRankBasic() => rankTests.DenseRankBasic();
 
     [Fact]
+    public void DenseRankEmptyOver() => rankTests.DenseRankEmptyOver();
+
+    [Fact]
     public void PercentRankBasic() => rankTests.PercentRankBasic(true);
+
+    [Fact]
+    public void PercentRankEmptyOver() => rankTests.PercentRankEmptyOver(true);
 }
