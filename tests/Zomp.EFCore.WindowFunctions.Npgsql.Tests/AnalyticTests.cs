9d89091c99998ed2f16f6ec3c2b71d80daebaa16
Victor Irzak <victor.irzak@zomp.com>
2023-12-20 14:59:07 -0500

    Add tests for other providers

---
+++ tests/Zomp.EFCore.WindowFunctions.Npgsql.Tests/AnalyticTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Zomp.EFCore.WindowFunctions.Npgsql.Tests;
+
+[Collection(nameof(NpgsqlCollection))]
+public class AnalyticTests : TestBase
+{
+    private readonly Testing.AnalyticTests analyticTests;
+
+    public AnalyticTests(ITestOutputHelper output)
+        : base(output)
+    {
+        analyticTests = new Testing.AnalyticTests(DbContext);
+    }
+
+    [Fact]
+    public void LeadBasic() => analyticTests.LeadBasic();
+}
---
+++ tests/Zomp.EFCore.WindowFunctions.Sqlite.Tests/AnalyticTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Zomp.EFCore.WindowFunctions.Sqlite.Tests;
+
+[Collection(nameof(SqliteCollection))]
+public class AnalyticTests : TestBase
+{
+    private readonly Testing.AnalyticTests analyticTests;
+
+    public AnalyticTests(ITestOutputHelper output)
+        : base(output)
+    {
+        analyticTests = new Testing.AnalyticTests(DbContext);
+    }
+
+    [Fact]
+    public void LeadBasic() => analyticTests.LeadBasic();
+}
