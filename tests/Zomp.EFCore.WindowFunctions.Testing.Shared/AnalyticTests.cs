63af8839bb096982f1b8a2d7829ec5a744159cf7
Victor Irzak <victor.irzak@zomp.com>
2023-12-25 00:49:28 -0500

    Add asserting to Lead test

--- tests/Zomp.EFCore.WindowFunctions.Testing.Shared/AnalyticTests.cs
+++ tests/Zomp.EFCore.WindowFunctions.Testing.Shared/AnalyticTests.cs
@@ -5,9 +5,14 @@ public partial class AnalyticTests
     [Fact]
     public void LeadBasic()
     {
+        const int offset = 2;
+        const int @default = 56;
         var query = DbContext.TestRows
-        .Select(r => EF.Functions.Lead(r.Id, 2, 56, EF.Functions.Over().OrderBy(r.Id).PartitionBy(r.Id / 10)));
+        .Select(r => EF.Functions.Lead(r.Id, offset, @default, EF.Functions.Over().OrderBy(r.Id).PartitionBy(r.Id / 10)));
 
         var result = query.ToList();
+
+        var expectedSequence = TestRows.Select((_, i) => i + offset >= TestRows.Length ? @default : (int?)TestRows[i + offset].Id);
+        Assert.Equal(expectedSequence, result);
     }
 }
