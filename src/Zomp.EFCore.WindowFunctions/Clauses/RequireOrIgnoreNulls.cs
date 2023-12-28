722712a489ee721a9d9e65d4aa31b3cbe71f96ad
Victor Irzak <victor.irzak@zomp.com>
2023-12-27 23:30:47 -0500

    Add RequireOrIgnoreNulls parameter

---
+++ src/Zomp.EFCore.WindowFunctions/Clauses/RequireOrIgnoreNulls.cs
@@ -0,0 +1,17 @@
+﻿namespace Zomp.EFCore.WindowFunctions.Clauses;
+
+/// <summary>
+/// Require Or Ignore Nulls.
+/// </summary>
+public enum RequireOrIgnoreNulls
+{
+    /// <summary>
+    /// Require nulls.
+    /// </summary>
+    RequireNulls,
+
+    /// <summary>
+    /// Ignore nulls.
+    /// </summary>
+    IgnoreNulls,
+}
