d5b49d1cd5010a758f5f70230912f0cf5cd353a7
Victor Irzak <victor.irzak@zomp.com>
2024-01-07 15:58:38 -0500

    Convert decimal to int when it is expected

---
+++ src/Zomp.EFCore.WindowFunctions.Oracle/Query/Internal/WindowFunctionsOracleTranslatorPluginFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace Zomp.EFCore.WindowFunctions.Oracle.Query.Internal;
+
+/// <summary>
+/// Window functions translator plugin factory for SQLite provider.
+/// </summary>
+public class WindowFunctionsOracleTranslatorPluginFactory : WindowFunctionsTranslatorPluginFactory
+{
+    private readonly ISqlExpressionFactory sqlExpressionFactory;
+    private readonly IRelationalTypeMappingSource relationalTypeMappingSource;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WindowFunctionsOracleTranslatorPluginFactory"/> class.
+    /// </summary>
+    /// <param name="sqlExpressionFactory">Instance of sql expression factory.</param>
+    /// <param name="relationalTypeMappingSource">Instance relational type mapping source.</param>
+    public WindowFunctionsOracleTranslatorPluginFactory(ISqlExpressionFactory sqlExpressionFactory, IRelationalTypeMappingSource relationalTypeMappingSource)
+        : base(sqlExpressionFactory, relationalTypeMappingSource)
+    {
+        this.sqlExpressionFactory = sqlExpressionFactory;
+        this.relationalTypeMappingSource = relationalTypeMappingSource;
+    }
+
+    /// <inheritdoc/>
+    public override WindowFunctionsTranslator Create()
+        => new WindowFunctionsOracleTranslator(sqlExpressionFactory, relationalTypeMappingSource);
+}
