8c4e2fb6b887330d990ebe9a50767c2586b2a742
Victor Irzak <victor.irzak@zomp.com>
2024-02-08 23:30:54 -0500

    Fix #12 Create test for Postgres ILike

---
+++ tests/Zomp.EFCore.WindowFunctions.Npgsql.Tests/NpgsqlSpecificTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Zomp.EFCore.WindowFunctions.Npgsql.Tests;
+
+[Collection(nameof(NpgsqlCollection))]
+public class NpgsqlSpecificTests(ITestOutputHelper output) : TestBase(output)
+{
+    [Fact]
+    public void Issue12BrokenILike()
+    {
+        var query = DbContext.TestRows
+            .Where(e => EF.Functions.ILike(e.Col1!.ToString()!, "%2%"));
+
+        var result = query.ToList();
+
+        var expected = TestRows.Where(t => t.Col1?.ToString()?.Contains('2', StringComparison.OrdinalIgnoreCase) ?? false);
+
+        Assert.Equal(expected, result, TestRowEqualityComparer.Default);
+    }
+}
