e75b9458e1f108493a239756b2f8d979304b11cc
Victor Irzak <victor.irzak@zomp.com>
2024-02-16 14:08:23 -0500

    Introduce rootless mode

--- .devcontainer/pgadmin4/config_local.py
+++ .devcontainer/pgadmin4/config_local.py
@@ -7,6 +7,6 @@ if not os.path.exists('/var/lib/pgadmin/storage/pgadmin4_pgadmin.org'):
     os.mkdir('/var/lib/pgadmin/storage/pgadmin4_pgadmin.org')
 
 pgpassfile = open('/var/lib/pgadmin/storage/pgadmin4_pgadmin.org/.pgpass','a+')
-pgpassfile.write("db:5432:npgsql_tests:npgsql_tests:npgsql_tests\n")
+pgpassfile.write("postgres:5432:npgsql_tests:npgsql_tests:npgsql_tests\n")
 pgpassfile.close()
 os.chmod('/var/lib/pgadmin/storage/pgadmin4_pgadmin.org/.pgpass', 0o600)
