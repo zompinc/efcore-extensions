edffc230bd30ab25de63b2626bff67423b9ee98b
Victor Irzak <victor.irzak@zomp.com>
2026-09-20 11:06:23 -0400

    Add failing tests for paging with window functions enabled on SQLite

--- tests/Zomp.EFCore.WindowFunctions.Sqlite.Tests/Partials.cs
+++ tests/Zomp.EFCore.WindowFunctions.Sqlite.Tests/Partials.cs
@@ -32,4 +32,7 @@ public partial class AnalyticTests(ITestOutputHelper output) : TestBase(output) 
 
 [Collection(nameof(SqliteCollection))]
 public partial class SubQueryTests(ITestOutputHelper output) : TestBase(output) { }
+
+[Collection(nameof(SqliteCollection))]
+public partial class PagingTests(ITestOutputHelper output) : TestBase(output) { }
 #pragma warning restore SA1402 // File may only contain a single type
